7997edae74f9b9473f98a11f67e52abd06687919
Dom <dominique.knockaert@orange.fr>
2026-08-21 17:41:58 +0200

    Ajout de la gestion des ingrédients et mise en forme

--- src/pages/Ajouter.jsx
+++ src/pages/Ajouter.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react"
+import { TrashIcon, PlusIcon } from "@phosphor-icons/react"
+
 export default function Ajouter() {
 
+    const [ingredient, setIngredient] = useState("")
+    const [ingredients, setIngredients] = useState([])
+
+    function ajouterIngredient() {
+
+        if (ingredient === "") {
+            return
+        }
+
+        if (ingredients.includes(ingredient)) {
+            return
+        }
+
+        setIngredients([...ingredients, ingredient])
+        setIngredient("")
+    }
+
+    function supprimerIngredient(ingredientASupprimer) {
+
+        const nouveauTableau = ingredients.filter(
+            (ingredient) => ingredient !== ingredientASupprimer
+        )
+
+        setIngredients(nouveauTableau)
+    }
+
     return(
-        <div>
+        <div className="ajouter">
             <h1>Ajouter des ingredients</h1>
+
+            <div className="ajout-formulaire">
+                <input className="ingredient-input" type="text" id="nom" name="nom" value={ingredient} onChange={(e) => {setIngredient(e.target.value)}} />
+
+
+
+                <button className="bouton-ajouter" onClick={ajouterIngredient }><PlusIcon /></button>
+            </div>
+            {ingredients.map((ingredient) => (
+            <div className="ingredient-item" key={ingredient}>
+                <p>{ingredient}</p>
+                <button className="bouton-supprimer" onClick={() => supprimerIngredient(ingredient)}><TrashIcon /></button>
+            </div>
+
+        ))}
         </div>
     )
+
+
 }
